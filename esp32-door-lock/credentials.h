58d45053db349a4f454e6c6d9999609d791c01e7
mmswflow-upb <152873071+mmswflow-upb@users.noreply.github.com>
2025-01-04 05:20:38 +0200

    Update credentials.h

--- esp32-door-lock/credentials.h
+++ esp32-door-lock/credentials.h
@@ -7,7 +7,7 @@ const char* password = "3TS9fFtZNf"; // Replace with your WiFi Password
 
 // Server Configuration
 const char* serverURL = "https://door-lock-notifier-c934666628c1.herokuapp.com";     
-const uint16_t serverPort = 27608;            // Replace with your server port
+const uint16_t serverPort = 8080;            // Replace with your server port
 const char* scanEndpoint = "/scan";          // Endpoint for scanning cards
 
 #endif // CREDENTIALS_H
